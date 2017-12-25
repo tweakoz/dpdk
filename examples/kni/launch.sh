3060cba4e2d25b52781865c246c3f514f755056b
michael t. mayers <michael@tweakoz.com>
2017-12-25 11:10:48 -0800

    launch script

---
+++ examples/kni/launch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+kernver=`uname -r`
+module=/lib/modules/${kernver}/extra/dpdk/rte_kni.ko
+ls -l ${module}
+sudo insmod ${module}
+build/kni -c 0x0f -n 2 -- -P -p 0x3 --config="(0,0,1),(1,2,3)" 
+
