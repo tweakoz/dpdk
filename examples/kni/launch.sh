12c69ed1ef5ec90d54ac99139248b945af6e6ced
michael t. mayers <michael@tweakoz.com>
2017-12-25 11:11:39 -0800

    ..

--- examples/kni/launch.sh
+++ examples/kni/launch.sh
@@ -4,4 +4,4 @@ module=/lib/modules/${kernver}/extra/dpdk/rte_kni.ko
 ls -l ${module}
 sudo insmod ${module}
 build/kni -c 0x0f -n 2 -- -P -p 0x3 --config="(0,0,1),(1,2,3)" 
-
+echo "http://hustcat.github.io/kni-in-dpdk/"
